4223ac24b09c74ff72f6cd9ff478cd1a8567d1c7
Matt Millican <matt@mattmillican.com>
2018-05-04 00:00:55 -0500

    Startin configuring API for AWS serverless deployment

---
+++ UniversityApi/LambdaFunction.cs
@@ -0,0 +1,16 @@
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace UniversityApi
+{
+    public class LambdaFunction : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .UseApiGateway();
+        }
+    }
+}
